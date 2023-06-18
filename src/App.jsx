a39943bbdf8870ce1a3954f53c5297705caf2277
Paulina <paulalastowiecka@gmail.com>
2023-06-18 14:06:58 +0200

    img update paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import "./App.css"
 import LoadingScreen from "./components/LoadingScreen"
 import Navigation from "./components/Navigation"
-import { useEffect, useState, useRef } from "react"
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Offers from "./pages/Offers"
 import Projects from "./pages/Projects"
 import Team from "./pages/Team"
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
+
 const App = () => {
 	const [isLoading, setIsLoading] = useState(true)
 
@@ -29,10 +30,8 @@ const App = () => {
 			) : (
 				<div className='app-page flex flex-col'>
 					<Navigation />
-					<Home />
-
 					<Routes>
-						<Route path='/' element={<Home />} />
+						<Route exact path='/' element={<Home />} />
 						<Route path='/offers' element={<Offers />} />
 						<Route path='/projects' element={<Projects />} />
 						<Route path='/team' element={<Team />} />
