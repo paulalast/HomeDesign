e64b29541ab6eb175b92c12a046040c962ee8fb0
Paulina <paulalastowiecka@gmail.com>
2023-06-18 14:51:04 +0200

    img update paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,14 @@ import "./App.css"
 import LoadingScreen from "./components/LoadingScreen"
 import Navigation from "./components/Navigation"
 import { useEffect, useState } from "react"
-import { Routes, Route, HashRouter } from "react-router-dom"
-
+import { Routes, Route } from "react-router-dom"
 import Offers from "./pages/Offers"
 import Projects from "./pages/Projects"
 import Team from "./pages/Team"
 import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 
-const router = createHashRouter([
-	{
-		path: "/*",
-		element: <App />,
-	},
-])
+
 const App = () => {
 	const [isLoading, setIsLoading] = useState(true)
 
@@ -31,22 +25,22 @@ const App = () => {
 	}, [])
 
 	return (
-		<HashRouter>
+		<main>
 			{isLoading ? (
 				<LoadingScreen />
 			) : (
 				<div className='app-page flex flex-col'>
 					<Navigation />
 					<Routes>
-						<Route exact path='/' element={<Home />} />
-						<Route path='/offers' element={<Offers />} />
+						<Route exact path='/' component={<Home />} />
+						<Route path='/offers' component={<Offers />} />
 						<Route path='/projects' element={<Projects />} />
 						<Route path='/team' element={<Team />} />
 						<Route path='/contact' element={<Contact />} />
 					</Routes>
 				</div>
 			)}
-		</HashRouter>
+		</main>
 	)
 }
 
