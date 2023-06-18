0b447e5fddd3f1212eedf71294e800e271d894fa
Paulina <paulalastowiecka@gmail.com>
2023-06-18 13:52:18 +0200

    img update paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import "./App.css"
 import LoadingScreen from "./components/LoadingScreen"
 import Navigation from "./components/Navigation"
 import { useEffect, useState, useRef } from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Offers from "./pages/Offers"
 import Projects from "./pages/Projects"
 import Team from "./pages/Team"
-import Contact from "/pages/Contact"
-import Home from "/pages/Home"
-
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
 const App = () => {
 	const [isLoading, setIsLoading] = useState(true)
 
@@ -30,12 +29,13 @@ const App = () => {
 			) : (
 				<div className='app-page flex flex-col'>
 					<Navigation />
-					<Home/>
+					<Home />
+
 					<Routes>
 						<Route path='/' element={<Home />} />
-						<Route path='./offers' element={<Offers />} />
-						<Route path='./projects' element={<Projects />} />
-						<Route path='./team' element={<Team />} />
+						<Route path='/offers' element={<Offers />} />
+						<Route path='/projects' element={<Projects />} />
+						<Route path='/team' element={<Team />} />
 						<Route path='/contact' element={<Contact />} />
 					</Routes>
 				</div>
